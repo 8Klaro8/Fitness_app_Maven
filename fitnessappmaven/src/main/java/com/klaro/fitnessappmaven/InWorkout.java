06f554792bf6201ba25438f2f4f44f1eb1fe6572
Robin Gerg <gergrobin@gmail.com>
2022-11-13 09:50:02 +0100

    Rewrote data storing into text

--- fitnessappmaven/src/main/java/com/klaro/fitnessappmaven/InWorkout.java
+++ fitnessappmaven/src/main/java/com/klaro/fitnessappmaven/InWorkout.java
@@ -26,17 +26,13 @@ public class InWorkout extends JFrame implements ActionListener, java.awt.event.
     JButton backButton, editWorkoutName, editWorkoutType, done, done2;
     JLabel workoutName, workoutType;
     JTextField workoutNameTextField, workoutTypeTextField;
-    String path, name, type;
+    String path, name, type, currentWorkoutName;
 
     ConnectToDB db = new ConnectToDB();
     Connection conn = db.connect_to_db("accounts", "postgres", System.getenv("PASSWORD"));
     CurrentUser currUser = new CurrentUser();
 
-    int elementCounter2;
-
-    public InWorkout(String clickedButton, int elementCounter) {
-
-        elementCounter2 = elementCounter;
+    public InWorkout(String clickedButton) {
         backButton = new JButton();
         editWorkoutName = new JButton("Edit Workout title");
         editWorkoutName
@@ -112,44 +108,35 @@ public class InWorkout extends JFrame implements ActionListener, java.awt.event.
 
         // Get current workout by workout's name
         try {
-            ArrayList<HashMap> workoutCollectionDelete;
-            String jsonData = db.read_workout(conn, currUser.get_current_user());
-            workoutCollectionDelete = prepare_workouts_by_selection(jsonData);
-
-            for (HashMap hashMap : workoutCollectionDelete) {
-                String formattedName = form_workout_name_from_db(hashMap); // remove '"' from 'name' value
-                if (formattedName.equals(String.valueOf(clickedButton))) {
-                    name = String.valueOf(hashMap.values().toArray()[0]);
-                    type = String.valueOf(hashMap.values().toArray()[1]);
-                    path = String.valueOf(hashMap.values().toArray()[2]);
-
-                    name = cut_quotes(name);
-                    type = cut_quotes(type);
-                    path = cut_quotes(path);
-
+            ArrayList<String> allWName = separate_collect_workout_datas(
+                    db.read_all_workout_name(conn, currUser.get_current_user()));
+            ArrayList<String> allWType = separate_collect_workout_datas(
+                    db.read_all_workout_type(conn, currUser.get_current_user()));
+            ArrayList<String> allWPath = separate_collect_workout_datas(
+                    db.read_all_workout_path(conn, currUser.get_current_user()));
+            for (int i = 0; i < allWName.size(); i++) {
+                if (allWName.get(i).equalsIgnoreCase(clickedButton)) {
+                    type = allWType.get(i);
+                    path = allWPath.get(i);
                     // display 'name', 'type' and 'path'
-                    workoutName = new JLabel(name, SwingConstants.CENTER);
+                    workoutName = new JLabel(clickedButton, SwingConstants.CENTER);
                     workoutType = new JLabel(type, SwingConstants.CENTER);
                     workoutName
                             .setFont(new Font(workoutName.getFont().getName(), workoutName.getFont().getStyle(), 30));
                     workoutType
                             .setFont(new Font(workoutType.getFont().getName(), workoutType.getFont().getStyle(), 30));
-
                     // set name
                     workoutName.setName("workoutLabel");
-
                     // add mouse listener
                     workoutName.addMouseListener(this);
                     // when clicked, make it editable
                     add_comp_to_centerPanel(path);
-
-                    System.out.println();
                 }
-
             }
-        } catch (Exception err) {
-            System.out.println(err.getMessage());
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
         }
+
     }
 
     private void add_comp_to_centerPanel(String path) {
@@ -158,88 +145,6 @@ public class InWorkout extends JFrame implements ActionListener, java.awt.event.
         panelCenter.add(editWorkoutName);
         panelCenter.add(workoutType);
         panelCenter.add(editWorkoutType);
-    }
-
-    public String cut_quotes(String input) {
-        return input.substring(1, input.length() - 1);
-    }
-
-    private String form_workout_name_from_db(HashMap hashMap) {
-        String nameFromDB = String.valueOf(hashMap.values().iterator().next());
-        return nameFromDB.substring(1, nameFromDB.length() - 1);
-    }
-
-    private ArrayList<HashMap> prepare_workouts_by_selection(String workouts) {
-        ArrayList<String> workoutSeparations = new ArrayList<String>(); // arraylist to contain separated workouts
-        String currentSubs = ""; // current substring
-        workoutSeparations = separate_workout(workoutSeparations, currentSubs, workouts); // separates each workout
-
-        // workout loop - as many times as many workouts given
-        ArrayList<String> pairCollections = new ArrayList<String>();
-        ArrayList<HashMap> workoutCollection = new ArrayList<HashMap>(); // collection of workout HashMaps
-
-        HashMap<String, String> currentWorkout = new HashMap<>(); // ready key value pairs
-        String currentPair = "";
-        for (int i = 0; i < workoutSeparations.size(); i++) { // number of workouts
-            currentPair = create_keyValue_pairs(workoutSeparations, pairCollections, currentPair, i);
-            pairCollections.add(currentPair); // add current workout pair to 'pairCollections'
-            currentPair = ""; // empty 'currentPair'
-            create_workout_pairs(pairCollections, currentWorkout);// read workout to add to UI, then clear ArrayList
-                                                                  // before add next workout.
-            pairCollections.clear(); // clear pairCollections after addition
-            workoutCollection.add(new HashMap<String, String>(currentWorkout)); // append currentWorkout as a new
-                                                                                // example to avoid 'reference trap'
-            currentWorkout.clear(); // after addition clear the 'container'
-        }
-        return workoutCollection;
-    }
-
-    private void create_workout_pairs(ArrayList<String> pairCollections, HashMap<String, String> currentWorkout) {
-        for (String pair : pairCollections) {
-            String[] entry = pair.split(":");
-            currentWorkout.put(entry[0].trim(), entry[1].trim());
-        }
-    }
-
-    private String create_keyValue_pairs(ArrayList<String> workoutSeparations, ArrayList<String> pairCollections,
-            String currentPair, int i) {
-        for (int k = 0; k < workoutSeparations.get(i).length(); k++) {
-            if (workoutSeparations.get(i).charAt(k) == ',') {
-                // add key, value pare to pairCollections
-                pairCollections.add(currentPair);
-                currentPair = "";
-            } else {
-                currentPair += String.valueOf(workoutSeparations.get(i).charAt(k));
-            }
-        }
-        return currentPair;
-    }
-
-    private ArrayList separate_workout(ArrayList<String> workoutSeparations, String currentSubs, String testString) {
-        /**
-         * Removes the brackets from the json string and separates the workouts by
-         * comma,
-         * then appends the separated results to 'workoutSeparations'
-         * 
-         * @param ArrayList<String> workoutSeparations
-         * @param String            currentSubs
-         * @param String            testString
-         */
-        int beginIndex = 0; // begin index to of the substing
-        int endIndex = 0;// end index to of the substing
-        for (int i = 0; i < testString.length(); i++) {
-            if (testString.charAt(i) == '{') {
-                beginIndex = i + 1;
-            } else if (testString.charAt(i) == '}') {
-                endIndex = (i);
-                currentSubs = testString.substring(beginIndex, endIndex);
-                workoutSeparations.add(currentSubs);
-                currentSubs = "";
-            } else {
-                currentSubs += String.valueOf(testString.charAt(i));
-            }
-        }
-        return workoutSeparations;
     }
 
     public JButton setWorkoutButtonIcon(String picPath, JButton button) {
@@ -265,13 +170,45 @@ public class InWorkout extends JFrame implements ActionListener, java.awt.event.
         }
     }
 
+    private ArrayList<String> separate_collect_workout_datas(String inputed_method) {
+        ArrayList<String> collectedWorkoutDatas = new ArrayList<String>();
+        try {
+            int begin = -1;
+            int end = -1;
+            int commaCounter = 0;
+            String currentSubString = "";
+            boolean beginAdded = false;
+            for (int i = 0; i < inputed_method.length(); i++) {
+                if (inputed_method.charAt(i) == ',') {
+                    if (!(beginAdded)) {
+                        begin = i + 1;
+                        beginAdded = true;
+                    }
+                    commaCounter++;
+                    if (commaCounter == 2) {
+                        end = i;
+                        currentSubString = inputed_method.substring(begin, end);
+                        begin = i + 1;
+                        commaCounter = 1;
+                        collectedWorkoutDatas.add(currentSubString.trim());
+                    }
+                }
+            }
+            return collectedWorkoutDatas;
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            System.out.println(e.getMessage());
+            return null;
+        }
+
+    }
+
     @Override
     public void actionPerformed(java.awt.event.ActionEvent e) {
         if (e.getSource() == backButton) {
             go_my_workouts();
         } else if (e.getSource() == editWorkoutName) {
-            System.out.println("Clicked");
-            String currentWorkoutName = workoutName.getText();
+            currentWorkoutName = workoutName.getText(); // get workout name
             workoutNameTextField = new JTextField(currentWorkoutName);
 
             // Remove comps.
@@ -318,9 +255,10 @@ public class InWorkout extends JFrame implements ActionListener, java.awt.event.
 
             panelCenter.revalidate();
             panelCenter.repaint();
-            try {
-                db.update_workout_name(conn, newWorkoutName, currUser.get_current_user(), workoutName.getText(), elementCounter2);
 
+            // TODO update workout name in db
+            try {
+                update_data_in_db(newWorkoutName);
             } catch (Exception err) {
                 System.out.println(err.getMessage());
             }
@@ -342,6 +280,28 @@ public class InWorkout extends JFrame implements ActionListener, java.awt.event.
             panelCenter.repaint();
         }
 
+    }
+    // TODO wrote it for edit type as well
+    private void update_data_in_db(String newWorkoutName) throws IOException {
+        ArrayList<String> allWName = separate_collect_workout_datas(
+                db.read_all_workout_name(conn, currUser.get_current_user()));
+        ArrayList<String> allWType = separate_collect_workout_datas(
+                db.read_all_workout_type(conn, currUser.get_current_user()));
+        ArrayList<String> allWPath = separate_collect_workout_datas(
+                db.read_all_workout_path(conn, currUser.get_current_user()));
+
+        for (int i = 0; i < allWName.size(); i++) {
+            if (allWName.get(i).equalsIgnoreCase(currentWorkoutName)) {
+                allWName.remove(i);
+                allWName.add(i, newWorkoutName);
+            }
+        }
+        db.remove_all_workout_data(conn, currUser.get_current_user());
+        for (int i = 0; i < allWName.size(); i++) {
+            db.add_workout_name(conn, allWName.get(i), currUser.get_current_user());
+            db.add_workout_type(conn, allWPath.get(i), currUser.get_current_user());
+            db.add_workout_path(conn, allWPath.get(i), currUser.get_current_user());
+        }
     }
 
     @Override
